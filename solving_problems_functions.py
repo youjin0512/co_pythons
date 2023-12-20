e303929248470e613698e83f517208528652b974
youjin0512 <kelly2.youjin.kim@gmail.com>
2023-12-20 07:05:40 +0000

    modify solving_problems_functions.py

--- solving_problems_functions.py
+++ solving_problems_functions.py
@@ -60,16 +60,17 @@ def problems_main(questions) :      # hint #탈자 추가 : problems_m => proble
 if __name__ == "__main__":
     # 출제 문제
     list_problems = [
-            {'Python에서 변수를 선언하는 방법은? (점수: 10점)'},  #각 라인별 {} 추가
-            {'Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)'},
-            {'1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다'}, 
-            {'Python에서 조건문을 작성하는 방법은? (점수: 10점)'}, 
-            {'1) if-else, 2) for-in, 3) while, 4) def'},
-            {'Python에서 함수를 정의하는 방법은? (점수: 5점)'}, 
-            {'1) class, 2) def, 3) import, 4) return'}
+            'Python에서 변수를 선언하는 방법은? (점수: 10점)',
+            '1) var name 2) name = value 3) set name 4) name == value',    # 누락된 답항 추가
+            'Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)',
+            '1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다', 
+            'Python에서 조건문을 작성하는 방법은? (점수: 10점)', 
+            '1) if-else, 2) for-in, 3) while, 4) def',
+            'Python에서 함수를 정의하는 방법은? (점수: 5점)', 
+            '1) class, 2) def, 3) import, 4) return'
     ]       # hint
     # 문제 당 정답
-    list_corrects = [4, 1, 1, 1 ]    # hint : 정답에 맞춰 수정
+    list_corrects = [2, 4, 4, 3 ]    # hint : [] 사이 정답 추가
 
 
     problems_main(list_problems) 
